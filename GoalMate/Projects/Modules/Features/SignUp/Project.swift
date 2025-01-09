0a1fce840e8924295f3e2ce2fa2cac668018fcef
importants <dlwognsdk@tukorea.ac.kr>
2025-01-09 10:34:17 +0900

    tuist: Login > SignUp으로 네이밍 변경

---
+++ GoalMate/Projects/Modules/Features/SignUp/Project.swift
@@ -0,0 +1,16 @@
+//
+//  Project.swift
+//  Config
+//
+//  Created by Importants on 1/2/25.
+//
+
+import TuistExtensions
+import ProjectDescription
+
+let featureSignUp = Project.framework(
+    name: "Feature" + Module.feature(.SignUp).name,
+    dependencies: [
+        Module.feature(.Common).project
+    ]
+)
