f13977a9787d192b95a231b9caf385eb52c503ff
Jae Hun Lee <118325810+importantsgit@users.noreply.github.com>
2025-02-05 04:39:58 +0900

    feat: SignUpFeatue, dependency 의존성 연결

--- GoalMate/Projects/Modules/Features/SignUp/Sources/Module/SignUp/SignUpFeature.swift
+++ GoalMate/Projects/Modules/Features/SignUp/Sources/Module/SignUp/SignUpFeature.swift
@@ -6,6 +6,9 @@
 //
 
 import ComposableArchitecture
+import Data
+import Dependencies
+import Utils
 
 @Reducer
 public struct SignUpFeature {
@@ -25,20 +28,44 @@ public struct SignUpFeature {
     public enum Action: BindableAction {
         case signUpButtonTapped(SignUpProvider)
         case signUpSucceeded
+        case signUpFailed
         case binding(BindingAction<State>)
     }
-
+    @Dependency(\.authClient) var authClient
     public var body: some Reducer<State, Action> {
         Reduce { state, action in
             switch action {
             case let .signUpButtonTapped(provider):
                 state.isLoading = true
-                print(provider)
-                return .run { send in
-                    try await Task.sleep(for: .seconds(3))
-                    await send.callAsFunction(.signUpSucceeded)
+                return .run { [provider] send in
+                    do {
+                        let result = try await (
+                            provider == .apple ?
+                                authClient.appleLogin() :
+                                authClient.kakaoLogin()
+                        )
+                        print(result)
+                        let tokens = try await authClient.authenticate(
+                            response: result,
+                            type: provider == .apple ? .apple : .kakao
+                        )
+                        print(tokens)
+                        await send(.signUpSucceeded)
+                    } catch let error as LoginError {
+                        print("Error(LoginError): \(error.localizedDescription)")
+                        await send(.signUpFailed)
+                    } catch let error as NetworkError {
+                        print("Error(NetworkError): \(error.localizedDescription)")
+                        print(error.localizedDescription)
+                        await send(.signUpFailed)
+                    } catch {
+                        print("Error: \(error.localizedDescription)")
+                    }
                 }
             case .signUpSucceeded:
+                state.isLoading = false
+                return .none
+            case .signUpFailed:
                 state.isLoading = false
                 return .none
             case .binding(_):
