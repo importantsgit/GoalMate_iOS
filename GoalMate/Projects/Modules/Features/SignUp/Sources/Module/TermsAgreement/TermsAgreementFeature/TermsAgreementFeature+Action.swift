111c6797e8727f01f39f25064eb04e41c358a8d2
Jae Hun Lee <118325810+importantsgit@users.noreply.github.com>
2025-02-10 04:01:25 +0900

    feat: 약관 동의 (Sheet) 뷰 추가
    추가 사항
    - View, Feature 추가

---
+++ GoalMate/Projects/Modules/Features/SignUp/Sources/Module/TermsAgreement/TermsAgreementFeature/TermsAgreementFeature+Action.swift
@@ -0,0 +1,60 @@
+//
+//  TermsAgreementFeature+Action.swift
+//  FeatureSignUp
+//
+//  Created by Importants on 2/9/25.
+//
+
+import ComposableArchitecture
+import Foundation
+import Utils
+
+extension TermsAgreementFeature {
+    func reduce(into state: inout State, action: ViewCyclingAction) -> Effect<Action> {
+        return .none
+    }
+    func reduce(into state: inout State, action: ViewAction) -> Effect<Action> {
+        switch action {
+        case .allAgreeButtonTapped:
+            state.isAllTermsAgreed.toggle()
+            state.isServiceTermsAgreed = state.isAllTermsAgreed
+            state.isPrivacyPolicyAgreed = state.isAllTermsAgreed
+            return .none
+        case .privacyPolicyAgreeButtonTapped:
+            state.isPrivacyPolicyAgreed.toggle()
+            if state.isServiceTermsAgreed &&
+                state.isPrivacyPolicyAgreed {
+                state.isAllTermsAgreed = true
+            }
+            return .none
+        case .termsOfServiceButtonTapped:
+            state.isServiceTermsAgreed.toggle()
+            if state.isServiceTermsAgreed &&
+                state.isPrivacyPolicyAgreed {
+                state.isAllTermsAgreed = true
+            }
+            return .none
+        case .openTermsOfServiceView:
+            guard let url = URL(
+                string: Environments.TermsOfServiceURL
+            )
+            else { return .none }
+            return .run { [url] _ in
+                _ = await openURL(url)
+            }
+        case .openPrivacyPolicyAgreeView:
+            guard let url = URL(
+                string: Environments.PrivacyPolicyAgreeURL
+            )
+            else { return .none }
+            return .run { [url] _ in
+                _ = await openURL(url)
+            }
+        case .startButtonTapped:
+            return .none
+        }
+    }
+    func reduce(into state: inout State, action: FeatureAction) -> Effect<Action> {
+        return .none
+    }
+}
