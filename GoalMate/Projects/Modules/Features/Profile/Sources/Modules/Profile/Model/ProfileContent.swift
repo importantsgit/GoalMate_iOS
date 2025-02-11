f06c8c73e56551294c97554760574984d190df58
Jae Hun Lee <118325810+importantsgit@users.noreply.github.com>
2025-02-12 03:05:11 +0900

    feat: Profile 내 표시될 Content 객체 멤버 추가
    추가 사항
    - inProgressCount: 진행 중인 목표 수
    - completedCount: 완료된 목표 수

--- GoalMate/Projects/Modules/Features/Profile/Sources/Modules/Profile/Model/ProfileContent.swift
+++ GoalMate/Projects/Modules/Features/Profile/Sources/Modules/Profile/Model/ProfileContent.swift
@@ -11,7 +11,6 @@ public struct ProfileContent: Identifiable, Equatable {
     public var id: String { name }
     var name: String
     var state: GoalCountState
-    
     init(
         name: String,
         state: GoalCountState
@@ -19,22 +18,31 @@ public struct ProfileContent: Identifiable, Equatable {
         self.name = name
         self.state = state
     }
-    
     public static func == (lhs: ProfileContent, rhs: ProfileContent) -> Bool {
         return lhs.id == rhs.id
     }
 }
 
 public struct GoalCountState {
-    // 진행 중
-    // 진행 완료
+    let inProgressCount: Int  // 진행 중인 목표 수
+    let completedCount: Int   // 완료된 목표 수
+    public init(
+        inProgressCount: Int,
+        completedCount: Int
+    ) {
+        self.inProgressCount = inProgressCount
+        self.completedCount = completedCount
+    }
 }
 
 extension ProfileContent {
     static var dummy: ProfileContent {
         ProfileContent(
             name: "김골메이트",
-            state: .init()
+            state: .init(
+                inProgressCount: 10,
+                completedCount: 2
+            )
         )
     }
 }
