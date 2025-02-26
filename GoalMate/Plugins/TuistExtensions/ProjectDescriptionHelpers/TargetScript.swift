becd14b3f7558c8c53ed54d6bb2c7bd5f4993abb
importants <dlwognsdk@tukorea.ac.kr>
2025-02-26 10:36:12 +0900

    tuist: TUIST_ROOT_DIR 환경 변수 값이 없을 시(tuist generate) 에러 메세지 노출

--- GoalMate/Plugins/TuistExtensions/ProjectDescriptionHelpers/TargetScript.swift
+++ GoalMate/Plugins/TuistExtensions/ProjectDescriptionHelpers/TargetScript.swift
@@ -18,6 +18,10 @@ public extension TargetScript {
             case .swiftLint:
                 return """
 ROOT_DIR=\(ProcessInfo.processInfo.environment["TUIST_ROOT_DIR"] ?? "")
+if [ -z "${ROOT_DIR}" ]; then
+  echo "error: 🚨 TUIST_ROOT_DIR 환경 변수가 설정되지 않았습니다! 'make generate' 명령을 사용하세요. 🚨"
+  exit 1
+fi
 echo "PROJECT_DIR: ${ROOT_DIR}"
 ${ROOT_DIR}/SupportFiles/Tools/swiftlint --config "${ROOT_DIR}/Projects/Modules/Resources/Resources/swiftlint.yml"
 """
