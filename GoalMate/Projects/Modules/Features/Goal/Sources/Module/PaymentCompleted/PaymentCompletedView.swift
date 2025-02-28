4f4e1608e741621a5fdfa095b9bda08fbf601ab2
importants <dlwognsdk@tukorea.ac.kr>
2025-02-28 15:34:08 +0900

    feat: 뷰 구조 변경

--- GoalMate/Projects/Modules/Features/Goal/Sources/Module/PaymentCompleted/PaymentCompletedView.swift
+++ GoalMate/Projects/Modules/Features/Goal/Sources/Module/PaymentCompleted/PaymentCompletedView.swift
@@ -28,80 +28,84 @@ struct PaymentCompletedView: View {
                     }
                 )
                 .frame(height: 64)
-                Spacer()
-                    .frame(height: 32)
-                Images.paymentCompleted
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .overlay {
-                        VStack {
-                            Spacer()
-                                .frame(height: 60)
-                            Text("반가워요,\n함께 목표까지 완주해요!")
-                                .pretendardStyle(
-                                    .semiBold,
-                                    size: 18,
-                                    color: Colors.grey900)
-                                .lineLimit(2)
-                                .multilineTextAlignment(.center)
-                            Spacer()
+                .padding(.leading, 4)
+                VStack(spacing: 0) {
+                    Spacer()
+                        .frame(height: 32)
+                    Images.paymentCompleted
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .overlay {
+                            VStack {
+                                Spacer()
+                                    .frame(height: 60)
+                                Text("반가워요,\n함께 목표까지 완주해요!")
+                                    .pretendardStyle(
+                                        .semiBold,
+                                        size: 18,
+                                        color: Colors.grey900)
+                                    .lineLimit(2)
+                                    .multilineTextAlignment(.center)
+                                Spacer()
+                            }
                         }
+                    Spacer()
+                        .frame(height: 16)
+                    VStack(alignment: .leading, spacing: 8) {
+                        HStack(alignment: .top, spacing: 30) {
+                            Text("목표")
+                                .pretendardStyle(.medium, size: 16, color: Colors.grey900)
+                            Text(store.content.title)
+                                .pretendardStyle(.semiBold, size: 16, color: Colors.grey900)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                        HStack(alignment: .top, spacing: 30) {
+                            Text("멘토")
+                                .pretendardStyle(.medium, size: 16, color: Colors.grey900)
+                            Text(store.content.mentor)
+                                .pretendardStyle(.semiBold, size: 16, color: Colors.grey900)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+    //                    HStack(alignment: .top, spacing: 30) {
+    //                        Text("가격")
+    //                            .pretendardStyle(.medium, size: 16, color: Colors.grey900)
+    //                        HStack(spacing: 10) {
+    //                            Text("\(store.content.originalPrice)원")
+    //                                .pretendardStyle(.semiBold, size: 16, color: Colors.grey900)
+    //                                .strikethrough(
+    //                                    color: Colors.grey900
+    //                                )
+    //                            Text("\(store.content.originalPrice)원")
+    //                                .pretendardStyle(.semiBold, size: 20, color: Colors.grey900)
+    //                        }
+    //                        .frame(maxWidth: .infinity, alignment: .leading)
+    //                    }
                     }
-                Spacer()
-                    .frame(height: 16)
-                VStack(alignment: .leading, spacing: 8) {
-                    HStack(alignment: .top, spacing: 30) {
-                        Text("목표")
-                            .pretendardStyle(.medium, size: 16, color: Colors.grey900)
-                        Text(store.content.title)
-                            .pretendardStyle(.semiBold, size: 16, color: Colors.grey900)
-                            .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 16)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 20)
+                            .stroke(Colors.grey300, lineWidth: 1)
+                    )
+                    Spacer()
+                    Button {
+                        store.send(.view(.startButtonTapped))
+                    } label: {
+                        Text("목표 시작하기")
+                            .pretendardStyle(
+                                .medium,
+                                size: 16,
+                                color: .black
+                            )
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 54)
+                            .background(Colors.primary)
+                            .clipShape(.capsule)
                     }
-                    HStack(alignment: .top, spacing: 30) {
-                        Text("멘토")
-                            .pretendardStyle(.medium, size: 16, color: Colors.grey900)
-                        Text(store.content.mentor)
-                            .pretendardStyle(.semiBold, size: 16, color: Colors.grey900)
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                    }
-//                    HStack(alignment: .top, spacing: 30) {
-//                        Text("가격")
-//                            .pretendardStyle(.medium, size: 16, color: Colors.grey900)
-//                        HStack(spacing: 10) {
-//                            Text("\(store.content.originalPrice)원")
-//                                .pretendardStyle(.semiBold, size: 16, color: Colors.grey900)
-//                                .strikethrough(
-//                                    color: Colors.grey900
-//                                )
-//                            Text("\(store.content.originalPrice)원")
-//                                .pretendardStyle(.semiBold, size: 20, color: Colors.grey900)
-//                        }
-//                        .frame(maxWidth: .infinity, alignment: .leading)
-//                    }
                 }
-                .padding(.horizontal, 20)
-                .padding(.vertical, 16)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 20)
-                        .stroke(Colors.grey300, lineWidth: 1)
-                )
-                Spacer()
-                Button {
-                    store.send(.view(.startButtonTapped))
-                } label: {
-                    Text("목표 시작하기")
-                        .pretendardStyle(
-                            .medium,
-                            size: 16,
-                            color: .black
-                        )
-                        .frame(maxWidth: .infinity)
-                        .frame(height: 54)
-                        .background(Colors.primary)
-                        .clipShape(.capsule)
-                }
+                .setMargin()
             }
-            .setMargin()
+            
         }
     }
 }
